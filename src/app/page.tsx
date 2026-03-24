9586878887080ab4d7864f883064ab27e6d1876a
ajay0550 <ajaygundu03@gmail.com>
2026-03-24 20:42:51 +0530

    Initial commit - portfolio website

---
+++ src/app/page.tsx
@@ -0,0 +1,74 @@
+import Experience from "@/components/Experience";
+import LinkWithIcon from "@/components/LinkWithIcon";
+import Posts from "@/components/Posts";
+import Projects from "@/components/Projects";
+import Socials from "@/components/Socials";
+import SwipeCards from "@/components/SwipeCards";
+import { Button } from "@/components/ui/Button";
+import { getPosts } from "@/lib/posts";
+import {
+  ArrowDown,
+  ArrowDownRight,
+  ArrowRightIcon,
+  FileDown,
+} from "lucide-react";
+import Link from "next/link";
+import path from "path";
+
+const blogDirectory = path.join(process.cwd(), "content");
+const TED_BIRTH_YEAR = 1997;
+const LIMIT = 2; // max show 2
+
+export default async function Home() {
+  const posts = await getPosts(blogDirectory, LIMIT);
+
+  return (
+    <article className="mt-8 flex flex-col gap-16 pb-16">
+      <section className="flex flex-col items-start gap-8 md:flex-row-reverse md:items-center md:justify-between">
+        <section></section>
+
+        <div className="flex max-w-[320px] flex-col sm:max-w-full">
+          <h1 className="title text-balance text-4xl sm:text-5xl">
+            hi Ajay here. 👋
+          </h1>
+
+          <p className="mt-2 whitespace-nowrap text-sm font-medium sm:text-base">
+          20yo aspiring software engineer
+          </p>
+
+          <p className="mt-4 max-w-sm text-balance text-sm sm:text-base">
+            Backend by trade, full-stack by passion. I build and self-host the
+            lot.
+          </p>
+
+          <section className="mt-6 flex flex-wrap items-center gap-4">
+            <Link href="/resume.pdf" target="_blank">
+              <Button variant="outline">
+                <span className="font-semibold">Resume</span>
+                <FileDown className="ml-2 size-5" />
+              </Button>
+            </Link>
+            <Socials />
+          </section>
+        </div>
+      </section>
+
+      <Experience />
+
+      <section className="flex flex-col gap-8">
+        <div className="flex justify-between">
+          <h2 className="title text-2xl sm:text-3xl">featured projects</h2>
+          <LinkWithIcon
+            href="/projects"
+            position="right"
+            icon={<ArrowRightIcon className="size-5" />}
+            text="view more"
+          />
+        </div>
+        <Projects limit={LIMIT} />
+      </section>
+
+      
+    </article>
+  );
+}
